6baa238ed485787e8b487c5d82e7ff916599f188
GeaninaCatalina <masgras.geanina@hotmail.com>
2020-06-01 17:11:49 +0200

    -m

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 import Description from './components/description/Description';
+import { Button, Icon } from 'semantic-ui-react';
 
 class App extends Component {
 
   render() {
     return (
       <div>
-        <Description></Description>
+        <div className='menu-container'>
+          <Button>Home</Button>
+          <Button>Projects</Button>
+        </div>
+          <Description></Description>
+        <footer className='footer-container'>
+          <h1>Find me on social media</h1>
+        </footer>
       </div>
     )
   }
--- src/components/description/Description.js
+++ src/components/description/Description.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
+import { Image, Card, Grid } from 'semantic-ui-react';
 import './Description.css';
-import { Card, Image } from 'semantic-ui-react';
+
 
 class Description extends Component {
 
   render() {
     return (
-      <div className='cover-container'> 
-        <img scr='/src/assets/images/description.jpg' alt='me'></img>
-
-        <p className='description-container' id='section1'>
-          I am a self-taught web developer, with a background in business analysis.
-          In this industry my mission is to help others by creating products that solve their real needs.
-              </p>
-        <p className='description-container'>
-          I will do this by being kind, servant and patient to listen and discover their problems.
-          By being clear, concise, though and stubborn I will make sure that I reach the goal of my projects.
-              </p>
-        <p className='description-container'>
-          I value kindness, honesty, and integrity because in my opinion are the most important values that need to exist in every relationship, no matter what.
-          Therefore, I will be honest and transparent even if things go wrong, so I can be a trusted partner in all aspects of my life.
-        </p>
+      <div className='cover-container'>
+          <Grid columns={2} relaxed='very' centered verticalAlign='middle'>
+            <Grid.Column textAlign='center' className='card-container' id='grid-cell'>
+                <Card className='card-container' id='card'>
+                  <Image src='/assets/images/description.jpg' />
+                  <Card.Content>
+                    <Card.Header>Geanina Catalina Vusca</Card.Header>
+                    <Card.Description>Web Developer</Card.Description>
+                  </Card.Content>
+                </Card>
+            </Grid.Column>
+            <Grid.Column textAlign='justified'>
+              <div className='description-container' id='description'>
+                <p className='description-container' id='section1'>
+                  I am a self-taught web developer, with a background in business analysis.
+                  In this industry my mission is to help others by creating products that solve their real needs.
+                </p>
+                <p className='description-container'>
+                  I will do this by being kind, servant and patient to listen and discover their problems.
+                  By being clear, concise, though and stubborn I will make sure that I reach the goal of my projects.
+                </p>
+                <p className='description-container'>
+                  I value kindness, honesty, and integrity because in my opinion are the most important values that need to exist in every relationship, no matter what.
+                  Therefore, I will be honest and transparent even if things go wrong, so I can be a trusted partner in all aspects of my life.
+                </p>
+              </div>
+            </Grid.Column>
+          </Grid>
       </div>
     )
   }
