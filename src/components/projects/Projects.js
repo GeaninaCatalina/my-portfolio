3750b88da469271afc685d7092397fd4904c9da0
GeaninaCatalina <masgras.geanina@hotmail.com>
2020-06-08 21:58:50 +0200

    add projects

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,16 +1,73 @@
 import React, { Component } from 'react';
-import { Grid, Segment } from 'semantic-ui-react';
+import { Grid, Image, GridRow } from 'semantic-ui-react';
 import './Projects.css';
 
 class Projects extends Component {
 
   render() {
     return (
-      <div>
-        <Segment className='segment-container'>
-          
-        </Segment>
-      </div>
+      <div className='container-projects'>
+        <div className='container-gridProjects'>
+          <Grid stackable columns={2} relaxed='very' className='container-gridProjects'>
+            <GridRow>
+              <Grid.Column width={7} >
+                <Image src='/assets/projects/playground.png' alt='messenger' size='large' />
+              </Grid.Column>
+              <Grid.Column width={9} textAlign='left'>
+                <p>
+                  Play Ground is a React application where I exercised  and learned basics algorithms with JavaScript.
+                  Based on the input, each button will return the result of an algorithm.
+                  </p>
+                <p>
+                  The most complex exercises are TicTacToe, Goimoku and Spare Matrix.
+                </p>
+              </Grid.Column>
+            </GridRow>
+         <GridRow>
+         <Grid.Column width={7} >
+                <Image src='/assets/projects/messenger.png' alt='messenger' size='large' />
+              </Grid.Column>
+              <Grid.Column width={9} textAlign='left'>
+                <p>
+                  React application where I exercised  and learned basics algorithms with JavaScript.
+                  Based on the input, each button will return the result of an algorithm.
+                  </p>
+                <p>
+                  The most complex exercises are TicTacToe, Goimoku and Spare Matrix.
+                </p>
+              </Grid.Column>
+            </GridRow>
+            <GridRow>
+         <Grid.Column width={7} >
+                <Image src='/assets/projects/calculator.png' alt='messenger' size='large' />
+              </Grid.Column>
+              <Grid.Column width={9} textAlign='left'>
+                <p>
+                  React application where I exercised  and learned basics algorithms with JavaScript.
+                  Based on the input, each button will return the result of an algorithm.
+                  </p>
+                <p>
+                  The most complex exercises are TicTacToe, Goimoku and Spare Matrix.
+                </p>
+              </Grid.Column>
+            </GridRow>
+            <GridRow>
+         <Grid.Column width={7} >
+                <Image src='/assets/projects/countdown.png' alt='messenger' size='large' />
+              </Grid.Column>
+              <Grid.Column width={9} textAlign='left'>
+                <p>
+                  React application where I exercised  and learned basics algorithms with JavaScript.
+                  Based on the input, each button will return the result of an algorithm.
+                  </p>
+                <p>
+                  The most complex exercises are TicTacToe, Goimoku and Spare Matrix.
+                </p>
+              </Grid.Column>
+            </GridRow>
+          </Grid>
+        </div>
+      </div >
     )
   }
 }
