77454be5ff899f8a3b3510024b1973b85174ac68
GeaninaCatalina <masgras.geanina@hotmail.com>
2020-06-16 20:47:10 +0200

    -m

--- src/components/description/Description.js
+++ src/components/description/Description.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Image, Grid, Label } from 'semantic-ui-react';
+import { Image, Grid, Label, GridRow, GridColumn } from 'semantic-ui-react';
 import './Description.css';
 
 const icons = ['js', 'react', 'css', 'html', 'sql', 'nodejs', 'github', 'semantic'];
@@ -9,72 +9,80 @@ class Description extends Component {
 
   render() {
     return (
-      <div className='cover-container' id='background'>
-        <Grid stackable columns={3} relaxed='very' className='cover-grid' centered>
-          <Grid.Column width={5} textAlign='center' className='card-container' id='grid-cell'>
-            <Grid stackable columns={2} relaxed verticalAlign='top'>
-              <Grid.Row>
-                <Grid.Column centered textAlign='center'>
-                  <h4>Hard Skils</h4>
-                  {icons.map(iconName => { return <Image src={'/assets/icons/' + iconName + '.png'} avatar size='mini' className='skils-icons' /> })}
-                </Grid.Column>
-                <Grid.Column stackable textAlign='left'>
-                  <h4>Soft Skils</h4>
-                  {lables.map(lable => { return <Label className='skils-lables'>{lable}</Label> })}
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-          </Grid.Column>
-          <Grid.Column width={5} className='card-container' id='grid-cell'>
-            <Grid stackable columns={1} relaxed centered>
-              <Grid.Row>
-                <Image src='/assets/images/description.jpg' alt='my-self' avatar size='medium' />
-              </Grid.Row>
-              <Grid.Row className='container-icon'>
-                <Image
-                  src='/assets/icons/github.png'
-                  id='icon'
-                  size='mini'
-                  href='https://github.com/GeaninaCatalina'
-                  target="_blank"
-                />
-                <Image src='/assets/icons/lin.png'
-                  id='icon'
-                  size='mini'
-                  href='https://www.linkedin.com/in/geanina-c%C4%83t%C4%83lina-masgras/'
-                  target="_blank"
-                />
-                <Image
-                  src='/assets/icons/twit.png'
-                  id='icon'
-                  size='mini'
-                  href='https://twitter.com/GeaninaMasgras'
-                  target="_blank"
-                />
-              </Grid.Row>
-            </Grid>
-          </Grid.Column>
-          <Grid.Column width={6} textAlign='justified'>
-            <div className='description-container' id='description'>
-              <p className='description-container' id='1'>
-                I am a self-taught web developer, with a background in functional analysis and BI.
+       
+        <div className='cover-container' id='background'>
+          <Grid stackable columns={3} relaxed='very' className='cover-grid' centered>
+            <GridRow columns={3} textAlign='center'>
+              <GridColumn width={5}></GridColumn>
+              <GridColumn width={5}>
+              <h2 className='title'>About</h2>
+              </GridColumn>
+              <GridColumn width={6}></GridColumn>
+            </GridRow>
+            <Grid.Column width={5} textAlign='center' className='card-container' id='grid-cell'>
+              <Grid stackable columns={2} relaxed verticalAlign='top'>
+                <Grid.Row>
+                  <Grid.Column centered textAlign='center'>
+                    <h4>Hard Skils</h4>
+                    {icons.map(iconName => { return <Image src={'/assets/icons/' + iconName + '.png'} avatar size='mini' className='skils-icons' /> })}
+                  </Grid.Column>
+                  <Grid.Column stackable textAlign='left'>
+                    <h4>Soft Skils</h4>
+                    {lables.map(lable => { return <Label className='skils-lables'>{lable}</Label> })}
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </Grid.Column>
+            <Grid.Column width={5} className='card-container' id='grid-cell'>
+              <Grid stackable columns={1} relaxed centered>
+                <Grid.Row>
+                  <Image src='/assets/images/description.jpg' alt='my-self' avatar size='medium' />
+                </Grid.Row>
+                <Grid.Row className='container-icon'>
+                  <Image
+                    src='/assets/icons/github.png'
+                    id='icon'
+                    size='mini'
+                    href='https://github.com/GeaninaCatalina'
+                    target="_blank"
+                  />
+                  <Image src='/assets/icons/lin.png'
+                    id='icon'
+                    size='mini'
+                    href='https://www.linkedin.com/in/geanina-c%C4%83t%C4%83lina-masgras/'
+                    target="_blank"
+                  />
+                  <Image
+                    src='/assets/icons/twit.png'
+                    id='icon'
+                    size='mini'
+                    href='https://twitter.com/GeaninaMasgras'
+                    target="_blank"
+                  />
+                </Grid.Row>
+              </Grid>
+            </Grid.Column>
+            <Grid.Column width={6} textAlign='justified'>
+              <div className='description-container' id='description'>
+                <p className='description-container' id='1'>
+                  I am a self-taught web developer, with a background in functional analysis and BI.
                 </p>
-              <p className='description-container' id='2'>
-                I started my career as a BI developer, to finally grow as a Business Analyst/PO.
-                After five years in this field, last year I decided that I want to know how the software products are actually build.
-                I started to learn web development and ended up loving it.
+                <p className='description-container' id='2'>
+                  I started my career as a BI developer, to finally grow as a Business Analyst/PO.
+                  After five years in this field, last year I decided that I want to know how the software products are actually build.
+                  I started to learn web development and ended up loving it.
               </p>
-              <p className='description-container' id='3'>
-                I invested in a few online courses and build web applications using technologies  as Javascript, NodeJs, React, CSS and HTML.
-                I was lucky enough to have a mentor during my journey who introduced me to Git, Github and who review my code and he has given me meaningful insights about a developer life.
+                <p className='description-container' id='3'>
+                  I invested in a few online courses and build web applications using technologies  as Javascript, NodeJs, React, CSS and HTML.
+                  I was lucky enough to have a mentor during my journey who introduced me to Git, Github and who review my code and he has given me meaningful insights about a developer life.
               </p>
-              <p className='description-container' id='3'>
-                Since I realised that this is what I want to do from now on, I coded  and learn nearly every day, no matter what because coding is now my biggest hobby and passion.
+                <p className='description-container' id='3'>
+                  Since I realised that this is what I want to do from now on, I coded  and learn nearly every day, no matter what because coding is now my biggest hobby and passion.
               </p>
-            </div>
-          </Grid.Column>
-        </Grid>
-      </div>
+              </div>
+            </Grid.Column>
+          </Grid>
+        </div>
     )
   }
 }
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Image, GridRow } from 'semantic-ui-react';
+import { Grid, Image, GridRow, GridColumn } from 'semantic-ui-react';
 import './Projects.css';
 
 class Projects extends Component {
@@ -9,91 +9,212 @@ class Projects extends Component {
       <div className='container-projects'>
         <div className='container-gridProjects'>
           <Grid stackable columns={2} relaxed='very' className='container-gridProjects'>
+            <GridRow columns={2}>
+              <GridColumn textAlign='center'>
+              <h2 className='title'>Projects</h2>
+              </GridColumn>
+              <GridColumn width={3}></GridColumn>
+            </GridRow>
+            {/* List */}
             <GridRow>
-              <Grid.Column width={7} >
-                <Image 
-                  src='/assets/projects/playground.png' 
-                  alt='playground' 
+              <Grid.Column width={7} verticalAlign='top'>
+                <Image
+                  src='/assets/projects/list.png'
+                  alt='list'
                   size='large'
                   className='container-image' id='playground'
-                  />
+                />
               </Grid.Column>
-              <Grid.Column width={9} textAlign='left'>
+              <Grid.Column width={9} textAlign='left' verticalAlign='top'>
                 <h3>Notes and Lists </h3>
                 <p>
-                  Play Ground is a React application where I exercised  and learned basics algorithms with JavaScript.
-                  Based on the input, each button will return the result of an algorithm.
-                  </p>
-                <p>
-                  The most complex exercises are TicTacToe, Gomoku and Spare Matrix.
+                  Notes and lists is an application build with React, NodeJS and MongoDB.
+                  I have created this application to learn how to work with MongoDB and Node Express.
                 </p>
+                <Grid columns={2} stackable>
+                  <Grid.Row>
+                    <Grid.Column>
+                      <h4>Functionalities:</h4>
+                      <ul>
+                        <li>Signup</li>
+                        <li>Login</li>
+                        <li>Add new list</li>
+                        <li>Copy lists</li>
+                        <li>Edit lists</li>
+                        <li>Delete lists </li>
+                      </ul>
+                    </Grid.Column>
+                    <Grid.Column>
+                      <h4>Technologies used:</h4>
+                      <ul>
+                        <li>React</li>
+                        <li>Axios</li>
+                        <li>i18next</li>
+                        <li>CSS</li>
+                        <li>Semantic-Ui </li>
+                        <li>NodeJs</li>
+                        <li>Router</li>
+                        <li>Express</li>
+                        <li>MongoDB</li>
+                        <li>Mongoose</li>
+                      </ul>
+                    </Grid.Column>
+                  </Grid.Row>
+                </Grid>
               </Grid.Column>
             </GridRow>
+            {/* Messenger */}
             <GridRow>
-              <Grid.Column width={7} >
+              <Grid.Column width={7} verticalAlign='top'>
                 <Image
                   src='/assets/projects/messenger.png'
                   alt='messenger'
                   size='large'
-                  className='container-image' id='messenger'
+                  className='container-image' id='playground'
                 />
               </Grid.Column>
-              <Grid.Column width={9} textAlign='left'>
+              <Grid.Column width={9} textAlign='left' verticalAlign='top'>
+                <h3>Messenger App </h3>
                 <p>
-                  React application where I exercised  and learned basics algorithms with JavaScript.
-                  Based on the input, each button will return the result of an algorithm.
-                  </p><p>
-                  The most complex exercises are TicTacToe, Goimoku and Spare Matrix.
-                </p><p>
-                  <ul>
-                    <li>React</li>
-                    <li>Semmantic-Ui</li>
-                    <li>Nodejs Express</li>
-                    <li>in18</li>
-                    <li>Axios</li>
-                    <li>Soket.io</li>
-                  </ul>
+                  Messenger app is created to allow communication between two persons.
+                  The purpose of this app was to get familiar with backend servers.
                 </p>
+                <Grid columns={2} stackable>
+                  <Grid.Row>
+                    <Grid.Column>
+                      <h4>Functionalities:</h4>
+                      <ul>
+                        <li>Signup</li>
+                        <li>Login</li>
+                        <li>Send messages </li>
+                        <li>Delete messages </li>
+                      </ul>
+                    </Grid.Column>
+                    <Grid.Column>
+                      <h4>Technologies used:</h4>
+                      <ul>
+                        <li>React</li>
+                        <li>Axios</li>
+                        <li>i18next</li>
+                        <li>Router</li>
+                        <li>CSS</li>
+                        <li>Semantic-Ui </li>
+                        <li>NodeJs</li>
+                        <li>Mongoose </li>
+                      </ul>
+                    </Grid.Column>
+                  </Grid.Row>
+                </Grid>
               </Grid.Column>
             </GridRow>
+            {/* Calculator */}
             <GridRow>
-              <Grid.Column width={7} >
+              <Grid.Column width={7} verticalAlign='top'>
                 <Image
-                  src='/assets/projects/calculator.png' 
-                  alt='calculator' 
-                  size='large' 
-                  className='container-image' id='calculator'
-                  />
-              </Grid.Column>
-              <Grid.Column width={9} textAlign='left'>
-                <p>
-                  React application where I exercised  and learned basics algorithms with JavaScript.
-                  Based on the input, each button will return the result of an algorithm.
-                  </p>
-                <p>
-                  The most complex exercises are TicTacToe, Goimoku and Spare Matrix.
-                </p>
-              </Grid.Column>
-            </GridRow>
-            <GridRow>
-              <Grid.Column width={7} >
-                <Image 
-                  src='/assets/projects/countdown.png'
-                  alt='countdown' 
+                  src='/assets/projects/calculator.png'
+                  alt='calculator'
                   size='large'
-                  className='container-image' id='countdown' 
-                  />
+                  className='container-image'
+                />
               </Grid.Column>
-              <Grid.Column width={9} textAlign='left'>
+              <Grid.Column width={9} textAlign='left' verticalAlign='top'>
+                <h3>Calculator </h3>
                 <p>
-                  React application where I exercised  and learned basics algorithms with JavaScript.
-                  Based on the input, each button will return the result of an algorithm.
-                  </p>
+                  I have created is a one page app to do basic mathematical operations.
+                </p>
+                <Grid columns={1} stackable>
+                  <Grid.Row>
+                    <Grid.Column>
+                      <h4>Technologies used:</h4>
+                      <ul>
+                        <li>React </li>
+                        <li>Semantic-Ui</li>
+                        <li>CSS</li>
+                      </ul>
+                    </Grid.Column>
+                  </Grid.Row>
+                </Grid>
+              </Grid.Column>
+            </GridRow>
+            {/* Countdown */}
+            <GridRow>
+              <Grid.Column width={7} verticalAlign='top'>
+                <Image
+                  src='/assets/projects/countdown.png'
+                  alt='countdown'
+                  size='large'
+                  className='container-image'
+                />
+              </Grid.Column>
+              <Grid.Column width={9} textAlign='left' verticalAlign='top'>
+                <h3>Countdown </h3>
                 <p>
-                  The most complex exercises are TicTacToe, Goimoku and Spare Matrix.
+                  I have created a one page app that simulates a countdown.
+                  The purpose of this was to get familiar with React.
+                </p>
+                <Grid columns={1} stackable>
+                  <Grid.Row>
+                    <Grid.Column>
+                      <h4>Technologies used:</h4>
+                      <ul>
+                        <li>React </li>
+                        <li>Semantic-Ui</li>
+                        <li>CSS</li>
+                      </ul>
+                    </Grid.Column>
+                  </Grid.Row>
+                </Grid>
+              </Grid.Column>
+            </GridRow>
+
+            {/* Playground */}
+            <GridRow>
+              <Grid.Column width={7} verticalAlign='top'>
+                <Image
+                  src='/assets/projects/playground.png'
+                  alt='playground'
+                  size='large'
+                  className='container-image'
+                />
+              </Grid.Column>
+              <Grid.Column width={9} textAlign='left' verticalAlign='top'>
+                <h3>Playground</h3>
+                <p>
+                  Playground is a React application where I exercised and learned basic algorithms with JavaScript.
+                  The most complex exercises were TicTacToe, Gomoku and Spare Matrix.
+                </p>
+                <Grid columns={1} stackable>
+                  <Grid.Row>
+                    <Grid.Column>
+                      <h4>Technologies used:</h4>
+                      <ul>
+                        <li>React </li>
+                        <li>JavaScript</li>
+                      </ul>
+                    </Grid.Column>
+                  </Grid.Row>
+                </Grid>
+              </Grid.Column>
+            </GridRow>
+            {/* Magic of cooking*/}
+            <GridRow>
+              <Grid.Column width={7} verticalAlign='top'>
+                <Image
+                  src='/assets/projects/cooking.png'
+                  alt='cooking'
+                  size='large'
+                  className='container-image'
+                />
+              </Grid.Column>
+              <Grid.Column width={9} textAlign='left' verticalAlign='top'>
+                <h3>Playground</h3>
+                <p>
+                  Playground is a React application where I exercised and learned basic algorithms with JavaScript.
+                  The most complex exercises were TicTacToe, Gomoku and Spare Matrix.
                 </p>
               </Grid.Column>
             </GridRow>
+
           </Grid>
         </div>
       </div >
