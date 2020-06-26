09687b7212920fcec9e95195761f1dea77bdb012
GeaninaCatalina <masgras.geanina@hotmail.com>
2020-06-26 22:18:16 +0200

    Add contact component

--- src/components/menu/contact/Contact.js
+++ src/components/menu/contact/Contact.js
@@ -9,21 +9,29 @@ class Description extends Component {
     return (
 
       <div className='cover-container'>
-        <Grid stackable columns={3} relaxed='very' className='contact-container' id='grid-contact' centered>
-          <GridRow columns={1}>
-            <GridColumn centered>
-            <h2 className='title'>Get in touch</h2>
+        <Grid stackable>
+          <GridRow columns={3}>
+            <GridColumn width={3}></GridColumn>
+            <GridColumn width={10}>
+              <Grid stackable columns={3} relaxed='very' className='contact-container' id='grid-contact' centered>
+                <GridRow columns={1}>
+                  <GridColumn centered>
+                    <h2 className='title'>Get in touch</h2>
+                  </GridColumn>
+                </GridRow>
+                <GridRow columns={2} textAlign='center'>
+                  <GridColumn textAlign='center'>
+                    <Image src={'/assets/icons/location.png'} size='mini' verticalAlign='middle' />{' '}
+                    <span> Leuven, Belgium</span>
+                  </GridColumn>
+                  <GridColumn textAlign='left'>
+                    <Image src={'/assets/icons/email.png'} size='mini' verticalAlign='middle' />
+                    <span>{' '} masgras.geanina@hotmail.com</span>
+                  </GridColumn>
+                </GridRow>
+              </Grid>
             </GridColumn>
-          </GridRow>
-          <GridRow columns={3} textAlign='center'>
-            <GridColumn width={8} centered> 
-            <h3> Location</h3>
-            <Image src={'/assets/icons/location.png'} size='mini' className='skils-icons' /> 
-            </GridColumn>
-            <GridColumn width={8} textAlign='center'>
-            <h3>Email</h3>
-            <Image src={'/assets/icons/email.png'} size='mini' className='skils-icons' /> 
-            </GridColumn>
+            <GridColumn width={3}></GridColumn>
           </GridRow>
         </Grid>
       </div>
