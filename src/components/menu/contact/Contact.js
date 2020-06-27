405fa7c62cc30b2f9ac255d6837475dac56638f3
GeaninaCatalina <masgras.geanina@hotmail.com>
2020-06-27 21:39:17 +0200

    finish contact page

--- src/components/menu/contact/Contact.js
+++ src/components/menu/contact/Contact.js
@@ -8,30 +8,32 @@ class Description extends Component {
   render() {
     return (
 
-      <div className='cover-container'>
-        <Grid stackable>
+      <div>
+        <Grid stackable id='contact-grid-container' >
           <GridRow columns={3}>
-            <GridColumn width={3}></GridColumn>
-            <GridColumn width={10}>
-              <Grid stackable columns={3} relaxed='very' className='contact-container' id='grid-contact' centered>
+            <GridColumn width={4}></GridColumn>
+            <GridColumn width={8} className='contact-container' id='grid-contact'>
+              <Grid stackable columns={3} relaxed='very'  centered>
                 <GridRow columns={1}>
-                  <GridColumn centered>
+                  <GridColumn centered textAlign='center'>
                     <h2 className='title'>Get in touch</h2>
+                    <h3>If you would like to have a discussion with me, don't hesitate to contact me.</h3>
                   </GridColumn>
                 </GridRow>
                 <GridRow columns={2} textAlign='center'>
                   <GridColumn textAlign='center'>
                     <Image src={'/assets/icons/location.png'} size='mini' verticalAlign='middle' />{' '}
-                    <span> Leuven, Belgium</span>
+                    <span>Leuven, Belgium</span>
+                    <h3></h3>
                   </GridColumn>
                   <GridColumn textAlign='left'>
                     <Image src={'/assets/icons/email.png'} size='mini' verticalAlign='middle' />
-                    <span>{' '} masgras.geanina@hotmail.com</span>
+                    <span>masgras.geanina@hotmail.com</span>
                   </GridColumn>
                 </GridRow>
               </Grid>
             </GridColumn>
-            <GridColumn width={3}></GridColumn>
+            <GridColumn width={4}></GridColumn>
           </GridRow>
         </Grid>
       </div>
