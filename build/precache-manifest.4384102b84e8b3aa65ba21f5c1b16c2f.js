32339c9049d254a381506efc3dd15d5339fb7a52
GeaninaCatalina <masgras.geanina@hotmail.com>
2020-06-30 21:04:01 +0200

    build

---
+++ build/precache-manifest.4384102b84e8b3aa65ba21f5c1b16c2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db23403814de8524d58f6c349586282f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "190781bc683ee83975df",
+    "url": "/static/css/main.1685e00b.chunk.css"
+  },
+  {
+    "revision": "d09a9b1920026057b24f",
+    "url": "/static/js/2.e7edf00c.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.e7edf00c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "190781bc683ee83975df",
+    "url": "/static/js/main.42b60e61.chunk.js"
+  },
+  {
+    "revision": "f6fac08a1749a9dbaa22",
+    "url": "/static/js/runtime-main.a106779c.js"
+  }
+]);
